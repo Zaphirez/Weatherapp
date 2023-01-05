42e1cbbf25e1daa0b82b67b821d14c88eb4d6d00
Zaphire <lucianotekhaus@hotmail.com>
2023-01-05 23:32:19 +0100

    Updated Comments

--- Weatherapp.py
+++ Weatherapp.py
@@ -5,7 +5,7 @@ import requests
 Country_Code = ''
 City_Name = ''
 
-
+# Function for getting individual Pieces of the input so it doesnt matter which way you input the Country Code and Name
 def separate_letters_and_numbers(lst):
     global Country_Code
     global City_Name
@@ -15,10 +15,11 @@ def separate_letters_and_numbers(lst):
         elif i.isalpha():
             City_Name += i
         else:
+            # If there are Letters in the Country Code or Numbers in the Country Name
             return 'Wrong Format'
 
 
-# My API Key get yours at the Website
+# My API Key get yours at the Website you can use mine tho
 apikey = "5df8688079f04145cb27c689c826c670"
 
 # Getting Long and Lat
@@ -27,6 +28,7 @@ split_input_Parameters = input_Parameters.split(", ")
 separate_letters_and_numbers(split_input_Parameters)
 loc = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={City_Name}, {Country_Code}&limit=5&appid={apikey}")
 data = loc.json()
+# print(data) # This is just for me so i can edit this later if there will be changes in the API Call
 lat = data[0]['lat']
 lon = data[0]['lon']
 
@@ -34,7 +36,7 @@ lon = data[0]['lon']
 request_weather = requests.get(
     f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}&units=metric")
 data_w = request_weather.json()
-# print(data_w) #DEBUG
+# print(data_w) # This is just for me so i can edit this later if there will be changes in the API Call
 
 # Getting data and Storing it to Variables
 w = data_w['weather'][0]['main']
@@ -43,8 +45,7 @@ Temp = data_w['main']['temp']
 fTemp = data_w['main']['feels_like']
 mTemp = data_w['main']['temp_min']
 maxTemp = data_w['main']['temp_max']
-wind_s = math.ceil(
-    int(data_w['wind']['speed']) * 1.852)  # Rounding the Numbers also Converting it to km/h del if miles
+wind_s = math.ceil(int(data_w['wind']['speed']) * 1.852)  # Rounding the Numbers also Converting it to km/h del if you want miles
 wind_d = int(data_w['wind']['deg'])  # Needing int for later
 hum = data_w['main']['humidity']
 
@@ -58,7 +59,7 @@ print(f"The current Weather is: {w} \n"
       f"Humidity is: {hum}% \n"
       f"Wind Speed is: {wind_s}km/h")
 
-# Making 360° also North, and yes you could just print out North but ist more fun this way :)
+# Making 360° = 0, and yes you could just print out North but its more fun this way :)
 if wind_d == 360:
     wind_d = 0
 # Getting the Wind Directions
